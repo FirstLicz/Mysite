e16a52b2668ccb2995b134039dc806e2866196cb
Leelcz <240953959@qq.com>
2018-02-06 15:54:18 +0800

    organization model设计

--- organization/models.py
+++ organization/models.py
@@ -14,9 +14,46 @@ from django.db import models
 '''
 
 
+class CityDict(models.Model):
+    name = models.CharField(max_length=50,verbose_name='城市')
+    desc = models.CharField(max_length=200,verbose_name='描述')
+    add_time = models.DateTimeField(default=datetime.now,verbose_name='添加时间')
+
+    class Meta:
+        verbose_name='城市'
+        verbose_name_plural=verbose_name
+
 class CourseOrg(models.Model):
 
-    name = models.CharField(verbose_name='课程机构名',max_length=50)
+    name = models.CharField(verbose_name='机构名称',max_length=50)
+    desc = models.TextField(verbose_name='机构描述')
+    click_nums = models.IntegerField(default=0,verbose_name='点击数')
+    fav_nums = models.IntegerField(default=0,verbose_name='收藏数')
+    image = models.ImageField(upload_to='courseorg/%Y/%m',verbose_name='封面图')
+    address = models.CharField(max_length=150,verbose_name='机构地址')
+    city = models.ForeignKey(CityDict,verbose_name='所在城市')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name='课程机构'
+        verbose_name_plural=verbose_name
+
+class Teacher(models.Model):
+
+    org = models.ForeignKey(CourseOrg,verbose_name='所属机构')
+    name = models.CharField(max_length=50,verbose_name='讲师名')
+    work_year = models.IntegerField(default=0,verbose_name='工作年限')
+    work_company = models.CharField(max_length=100,verbose_name='所在单位')
+    work_position = models.CharField(max_length=50,verbose_name='就职名称')
+    domain = models.CharField(max_length=50,verbose_name='教学领域')
+    click_nums = models.IntegerField(default=0,verbose_name='点击数')
+    fav_nums = models.IntegerField(default=0,verbose_name='收藏数')
+    add_time = models.DateTimeField(default=datetime.now,verbose_name='添加时间')
+
+    class Meta:
+        verbose_name='讲师'
+        verbose_name_plural=verbose_name
+
 
 
 
