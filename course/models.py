5fc0a9ba7a1f5325d5458f3df1cef481962f53c1
Leelcz <240953959@qq.com>
2018-02-06 10:15:01 +0800

    course model设计

--- course/models.py
+++ course/models.py
@@ -5,6 +5,13 @@ from datetime import datetime
 from django.db import models
 
 # Create your models here.
+'''
+    course          课程基本信息
+    lession         章节信息
+    video           视频信息
+    courseresource  课程资源
+'''
+
 
 DEGREE=(
     ('cj','初级'),
@@ -21,11 +28,51 @@ class Course(models.Model):
     learn_times = models.IntegerField(default=0,verbose_name='学习时长',help_text='存储单位是分钟')
     students = models.IntegerField(default=0,verbose_name='学习人数')
     fav_nums = models.IntegerField(default=0,verbose_name='收藏人数')
-    image = models.ImageField(upload_to='course/%Y/%m',verbose_name='课程封面')
+    image = models.ImageField(max_length=200,upload_to='course/%Y/%m',verbose_name='课程封面')
     click_nums = models.IntegerField(default=0,verbose_name='点击数')
     add_time = models.DateTimeField(default=datetime.now,verbose_name='添加时间')
 
     class Meta:
         verbose_name='课程'
         verbose_name_plural=verbose_name
+
+
+class Lession(models.Model):
+
+    course = models.ForeignKey(Course,verbose_name='课程')
+    name = models.CharField(max_length=100,verbose_name='章节名',null=False,blank=False)
+    add_time = models.DateTimeField(default=datetime.now,verbose_name='添加时间')
+
+    class Meta:
+        verbose_name='章节'
+        verbose_name_plural=verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+class Vide(models.Model):
+
+    lession = models.ForeignKey(Lession,verbose_name='章节')
+    name = models.CharField(max_length=50,verbose_name='视频名')
+    video_url = models.FileField(max_length=200,upload_to='course/video/%Y/%m',verbose_name='视频',help_text='视频路劲')
+    add_time = models.DateTimeField(default=datetime.now,verbose_name='添加时间')
+
+    class Meta:
+        verbose_name='视频'
+        verbose_name_plural=verbose_name
+
+
+class CourseResource(models.Model):
+
+    course = models.ForeignKey(Course,verbose_name='课程')
+    name = models.CharField(max_length=100,verbose_name='资源名')
+    resource_file = models.FileField(max_length=200,upload_to='course/resource/%Y/%m',verbose_name='资源文件')
+    add_time = models.DateTimeField(default=datetime.now,verbose_name='添加时间')
+
+    class Meta:
+        verbose_name='课程资源'
+        verbose_name_plural=verbose_name
+
+
 
