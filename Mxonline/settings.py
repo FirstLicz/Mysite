939891d691a5c8791a78f144ac6e7462839158a3
Leelcz <240953959@qq.com>
2018-02-07 18:03:17 +0800

    后台全局配置

--- Mxonline/settings.py
+++ Mxonline/settings.py
@@ -132,6 +132,15 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR,'static'),
+
+)
+
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
 
